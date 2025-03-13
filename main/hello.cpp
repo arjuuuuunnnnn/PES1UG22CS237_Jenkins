99f44ec3337f2b81790eb03a333d9b9d034b059d
arjuuuuunnnnn <arjunbanur27@gmail.com>
2025-03-13 10:39:23 +0530

    changed2

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I made a change\n";
+  cout << "I made a change haha\n";
   return 0;
 }
